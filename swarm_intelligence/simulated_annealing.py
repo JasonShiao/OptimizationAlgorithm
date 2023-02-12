f534d66507b9b8f4564c3cd5b45d93ceccf81627
Jason Shiao <b01901105@ntu.edu.tw>
2023-02-13 02:47:33 +0800

    Fixup

--- swarm_intelligence/simulated_annealing.py
+++ swarm_intelligence/simulated_annealing.py
@@ -45,17 +45,13 @@ class SA:
         """
             Determine whether accept the new solution
         """
-        if delta_e > 0:
-            prob = exp(-delta_e / self.temp)
-            print(f'prob: {prob}')
-        return (delta_e <= 0 or prob > random())
-        #return (delta_e <= 0 or 
-        #        exp(-delta_e / self.temp) > random())
+        return (delta_e <= 0 or 
+                exp(-delta_e / self.temp) > random())
     
     def annealing(self):
-        # cool by scale
+        # Annealing type 1: cool by scale
         self.temp *= self.cooling_rate
-        # cool by constant temp
+        # Annealing type 2: cool by constant temp
         # self.temp -= self.cooling_rate
     
     def optimize(self, objective_func, num_var, initial_solution, constraint):
@@ -70,11 +66,7 @@ class SA:
             # 2. Evaluate
             new_energy = objective_func(new_solution)
             delta_e = new_energy - self.energy
-            #print(f'new solution: {self.solution}')
-            print(f'new energy: {new_energy}, last energy: {self.energy}, delta E: {delta_e}')
-            print(f'iter-k: {self.iter}')
             if self.check_accept(delta_e):
-                print('accept')
                 self.solution = new_solution
                 self.energy = new_energy
             # 3. Check reach max iter for current temperature
@@ -91,6 +83,5 @@ class SA:
                 self.iter += 1
                 # TODO: Next step with generating function
                 new_solution = self.gen_func(self.solution)
-            print('-----------------------')
         return self.solution, self.energy
         
