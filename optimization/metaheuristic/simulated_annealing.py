15c18710fde102dc08a09a5fb0dc395f4ad61a93
Jason Shiao <b01901105@ntu.edu.tw>
2023-12-05 23:03:04 +0800

    Added example for simulated annealing

--- optimization/metaheuristic/simulated_annealing.py
+++ optimization/metaheuristic/simulated_annealing.py
@@ -1,6 +1,16 @@
 from math import exp, inf
-from random import random, gauss
+from random import random
+from enum import Enum
 import numpy as np
+import benchmark_functions as bf
+
+class NextStepPolicy(Enum):
+    Gaussian = 'gaussian'
+    Cauchy = 'cauchy'
+    
+class CoolingSchedule(Enum):
+    Scale = 'scale'
+    Constant = 'constant'
 
 class SA:
     """ 
@@ -12,50 +22,68 @@ class SA:
     temp = 0
     history = []
     
-    def __init__(self, init_temp, terminate_temp, cooling_rate, max_iter, gen_func):
+    def __init__(self, init_temp: float, terminate_temp: float, cooling_sched: CoolingSchedule, cooling_rate: float,
+                 max_epoch: int, max_iter_per_temp: int, next_step_policy = NextStepPolicy.Cauchy):
         self.init_temp = init_temp
         self.terminate_temp = terminate_temp
         if init_temp < terminate_temp:
             raise ValueError("Initial temp must be higher than terminate temperature")
+        self.cooling_sched = cooling_sched
         self.cooling_rate = cooling_rate
-        self.max_iter = max_iter # Max iteration for "a temperature"
-        if gen_func == 'gaussian':
-            self.gen_func = self.gaussian_gen_func
-        elif gen_func =='cauchy':
-            self.gen_func = self.cauchy_gen_func
+        self.max_epoch = max_epoch
+        self.max_iter_per_temp = max_iter_per_temp # Max iteration for "a temperature"
+        if next_step_policy in [NextStepPolicy.Gaussian, NextStepPolicy.Cauchy]:
+            self.next_step_policy = next_step_policy
         else:
             raise ValueError("gen_func must be 'gaussian' or 'cauchy'")
     
-    def gaussian_gen_func(self, x, bound_min, bound_max):
+    def next_step(self, current_sol, bound_min, bound_max):
         """
             Generate random solution with multivariate Gaussian distribution
                 mean: current solution: x
                 sigma: temperature -> covariate matrix (for multivariate gaussian)
+            
+            Gaussian (Normal) distribution has a lower peak and thinner tails than Cauchy distribution
+            which makes it more prone to be trapped in local optima
+            
+            Cauchy-Lorentzian distribution has a higher peak and thicker tails than Gaussian distribution
+            Thus, it is more likely to trigger a long jump and exit the local optima
         """
-        current_x = np.array(x)
-        cov = np.identity(current_x.size) * self.temp
-        next_x = np.random.multivariate_normal(x, cov, size=1)[0]
-        # Apply constraint (bounds)
-        next_x = np.clip(next_x, a_min = np.array(bound_min), a_max = np.array(bound_max))
-        return next_x.tolist()
-
-    def cauchy_gen_func(self, x, bound_min, bound_max):
-        pass
+        if self.next_step_policy == NextStepPolicy.Gaussian:
+            current_x = np.array(current_sol)
+            cov = np.identity(current_x.size) * self.temp
+            next_x = np.random.multivariate_normal(current_sol, cov, size=1)[0]
+            # Apply constraint (bounds)
+            next_x = np.clip(next_x, a_min = np.array(bound_min), a_max = np.array(bound_max))
+            return next_x.tolist()
+        elif self.next_step_policy == NextStepPolicy.Cauchy:
+            cauchy_rand = np.random.standard_cauchy(len(current_sol))
+            next_x = cauchy_rand * self.temp + np.array(current_sol)
+            # Apply constraint (bounds)
+            next_x = np.clip(next_x, a_min = np.array(bound_min), a_max = np.array(bound_max))
+            return next_x.tolist()
+        else:
+            raise ValueError("Unknown next step policy")
     
     def check_accept(self, delta_e):
         """
-            Determine whether accept the new solution
+            Determine whether accept the new solution:
+            delta_e <= 0: means new solution is better -> always accept
+            delta_e > 0: means new solution is worse -> accept with probability exp(-delta_e / temp)
         """
-        return (delta_e <= 0 or 
-                exp(-delta_e / self.temp) > random())
+        return (delta_e <= 0 or random() < exp(-delta_e / self.temp))
     
     def annealing(self):
-        # Annealing type 1: cool by scale
-        self.temp *= self.cooling_rate
-        # Annealing type 2: cool by constant temp
-        # self.temp -= self.cooling_rate
+        if self.cooling_sched == CoolingSchedule.Scale:
+            # Annealing type 1: cool by scale
+            self.temp *= self.cooling_rate
+        elif self.cooling_sched == CoolingSchedule.Constant:
+            # Annealing type 2: cool by constant temp
+            self.temp -= self.cooling_rate
+        else:
+            raise ValueError("Unknown cooling schedule")        
     
-    def optimize(self, objective_func, n_dim, initial_solution = None):
+    def optimize(self, objective_func: bf.BenchmarkFunction, n_dim, verbose = False):
         """_summary_
 
         Args:
@@ -66,36 +94,44 @@ class SA:
         Returns:
             _type_: _description_
         """
-        self.iter = 1
-        self.temp = self.init_temp
-        self.solution = None
-        self.energy = inf
         # 1. Initialize a random solution
         min_bounds, max_bounds = objective_func.suggested_bounds()
-        if not initial_solution:
-            new_solution = [random() * (max_bounds[idx] - min_bounds[idx]) + min_bounds[idx] for idx in range(n_dim)]
-        else:
-            new_solution = initial_solution
-        while True:
-            # 2. Evaluate
-            new_energy = objective_func(new_solution)
-            delta_e = new_energy - self.energy
-            if self.check_accept(delta_e):
-                self.solution = new_solution
-                self.energy = new_energy
-            # 3. Check reach max iter for current temperature
-            if self.iter >= self.max_iter:
-                # If reach terminate temperature -> terminate
-                if self.temp < self.terminate_temp:
-                    break
+        epoch = 0
+        best_so_far_solution = None
+        best_so_far_energy = np.inf
+        while epoch < self.max_epoch:
+            self.iter = 1
+            self.temp = self.init_temp
+            self.solution = None
+            self.energy = inf
+            new_solution = list(np.random.uniform(low=min_bounds, high=max_bounds, size=n_dim))
+            while True:
+                # 2. Evaluate
+                new_energy = objective_func(new_solution)
+                if new_energy == None:
+                    raise ValueError("Objective function must return a value")
+                delta_e = new_energy - self.energy
+                if self.check_accept(delta_e):
+                    if new_energy < best_so_far_energy:
+                        best_so_far_energy = new_energy
+                        best_so_far_solution = new_solution
+                    self.solution = new_solution
+                    self.energy = new_energy
+                # 3. Check reach max iter for current temperature
+                if self.iter >= self.max_iter_per_temp:
+                    # If reach terminate temperature -> terminate
+                    if self.temp < self.terminate_temp:
+                        break
+                    else:
+                        self.annealing()
+                        self.iter = 1
+                        # TODO: Next step with generating function
+                        new_solution = self.next_step(self.solution, min_bounds, max_bounds)
                 else:
-                    self.annealing()
-                    self.iter = 1
+                    self.iter += 1
                     # TODO: Next step with generating function
-                    new_solution = self.gen_func(self.solution, min_bounds, max_bounds)
-            else:
-                self.iter += 1
-                # TODO: Next step with generating function
-                new_solution = self.gen_func(self.solution, min_bounds, max_bounds)
-        return self.solution, self.energy
-        
+                    new_solution = self.next_step(self.solution, min_bounds, max_bounds)
+            epoch += 1
+            if verbose:
+                print(f"Epoch {epoch} best solution = {best_so_far_solution}, best energy = {best_so_far_energy}")
+        return best_so_far_solution, best_so_far_energy
